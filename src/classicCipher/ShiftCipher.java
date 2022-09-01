ffdd66a3f5b4099a41f15cf1426da83d4621ea30
Mohammed Rahman <minhaz357@gmail.com>
2022-08-31 22:07:52 -0400

    Rename variables and functions

--- src/classicCipher/ShiftCipher.java
+++ src/classicCipher/ShiftCipher.java
@@ -1,14 +1,13 @@
 package classicCipher;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 public class ShiftCipher {
 
     // Given a ciphertext, try decrypt with every possible key
     // Only one possibility will “make sense”
-    public static void decryptCipherText(Map<Character, Integer> letters, String cipherText){
+    public static void decryptCipherText(Map<Character, Integer> letterPositionMap, String cipherText){
         // key space K = {0...25}
         int[] keySpace = new int[26];
         for (int key = 0; key < 26; key++){
@@ -19,56 +18,56 @@ public class ShiftCipher {
 
         // decrypt the ciphertext using all the possible 26 keys
         for (int key: keySpace){
-            String message = decrypt(letters, key, lowerCaseCipherText);
+            String message = decrypt(letterPositionMap, key, lowerCaseCipherText);
             System.out.println("key: " + key + " message: " + message);
         }
     }
-    public static String decrypt(Map<Character, Integer> letters, int key, String cipherText){
+    public static String decrypt(Map<Character, Integer> letterPositionMap, int key, String cipherText){
         StringBuilder message = new StringBuilder();
 
         // shift each letter of the ciphertext backward by k position
         for (char ch: cipherText.toCharArray()){
-            int complement = letters.get(ch) - key;
+            int complement = letterPositionMap.get(ch) - key;
             if (complement < 0){ // if the complement is negative add 26 to it.
                 complement += 26;
             }
             int position = complement % 26;
-           char messageLetter = getCipherLetter(position, letters);
+           char messageLetter = getLetterAfterShifting(position, letterPositionMap);
             message.append(messageLetter);
         }
         return message.toString();
     }
 
-    public static String encrypt(int key, String message, Map<Character, Integer> letters){
+    public static String encrypt(int key, String message, Map<Character, Integer> letterPositionMap){
         StringBuilder cipherText = new StringBuilder();
 
         // iterate through the message and shift each letter by k position
         for (char ch: message.toCharArray()){
-            int shiftedPosition = (letters.get(ch) + key) % 26;
-            char cipherLetter = getCipherLetter(shiftedPosition, letters);
+            int shiftedPosition = (letterPositionMap.get(ch) + key) % 26;
+            char cipherLetter = getLetterAfterShifting(shiftedPosition, letterPositionMap);
             cipherText.append(cipherLetter);
         }
 
         return cipherText.toString();
     }
 
-    public static Character getCipherLetter(int val, Map<Character, Integer> letters){
-        for (Map.Entry<Character, Integer> entry: letters.entrySet()){
-          if (entry.getValue() == val) return entry.getKey();
+    public static Character getLetterAfterShifting(int position, Map<Character, Integer> letterPositionMap){
+        for (Map.Entry<Character, Integer> entry: letterPositionMap.entrySet()){
+          if (entry.getValue() == position) return entry.getKey();
         }
         return 0;
     }
     public static void main(String[] args) {
-        Map<Character, Integer> letters = new HashMap<>();
-        int i = 0;
+        Map<Character, Integer> letterPositionMap = new HashMap<>();
+        int position = 0;
         for (char ch = 'a'; ch <= 'z'; ch++){
-            letters.put(ch, i);
-            i++;
+            letterPositionMap.put(ch, position);
+            position++;
         }
 
         String message = "begintheattacknow";
         // call the encryption method
-        System.out.println("Cipher Text: " + encrypt(3, message, letters));
+        System.out.println("Cipher Text: " + encrypt(3, message, letterPositionMap));
 
         // call the decryption method
         /** Ciphertext example:
@@ -77,6 +76,6 @@ public class ShiftCipher {
          * OVDTHUFWVZZPISLRLFZHYLAOLYL
          * */
         // only one possible outcome will make sense
-        decryptCipherText(letters, "OVDTHUFWVZZPISLRLFZHYLAOLYL");
+        decryptCipherText(letterPositionMap, "OVDTHUFWVZZPISLRLFZHYLAOLYL");
     }
 }
